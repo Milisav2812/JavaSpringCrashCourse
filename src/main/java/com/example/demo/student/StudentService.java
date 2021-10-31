26e32504c00ba0d71ab41e628925dde5b22996f2
Milisav2812 <milisaw95@gmail.com>
2021-10-31 10:11:22 +0100

    # Service Layer created - Dependency Injection implemented

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(1L, "Milos", 25, LocalDate.of(1995, 12,28), "milos@gmail.com")
+        );
+    }
+}
